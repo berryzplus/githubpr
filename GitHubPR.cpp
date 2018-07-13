824e82565b0b8fab0113afb613a6a10cc9168734
berryplus <berryzplus@gmail.com>
2018-07-13 21:53:33 +0900

    Add project files.

---
+++ GitHubPR.cpp
@@ -0,0 +1,12 @@
+﻿// GitHubPR.cpp : Defines the entry point for the application.
+//
+
+#include "GitHubPR.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
