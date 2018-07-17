58b424292c64f20ad47bce3514214feef97534ee
berryzplus <berryzplus@gmail.com>
2018-07-17 19:31:58 +0900

    wmain関数を独立ファイルに分離
    単体テストを書きやすくするための変更。

--- src/GitHubPR.cpp
+++ src/GitHubPR.cpp
@@ -17,32 +17,6 @@ const WCHAR BRANCH_PREFIX[] = L"BRANCH_PREFIX";
 const WCHAR PR_NUMBER[] = L"PR_NUMBER";
 
 
-int wmain(int argc, WCHAR* argv[])
-{
-	GitHubPrApp app;
-	try {
-		// 最初の引数を取り出す
-		LPCWSTR arg = (1 < argc && argv[1][0] != '\0') ? argv[1] : L"";
-
-		// 初期化する
-		app.initSettings(arg);
-
-		//バッチファイルを実行する
-		app.executeBatch();
-
-		//ユーザ入力を待機する(読み捨て)
-		std::wcout << loadString(IDS_PRESS_ANY_KEY) << std::endl;
-		std::wcin.get();
-	}
-	catch (const std::runtime_error &e) {
-		const char* what = e.what();
-		const size_t cbWhat = ::strlen(what);
-		::MessageBox(NULL, convertMbsToWString(what, cbWhat).c_str(), GITHUB_PR, MB_OK);
-	}
-
-	return 0;
-}
-
 void GitHubPrApp::initSettings(_In_ const std::wstring &targetDir)
 {
 	::SetConsoleTitle(GITHUB_PR);
---
+++ src/wmain.cpp
@@ -0,0 +1,33 @@
+﻿
+#include "StdAfx.h"
+#include "GitHubPR.h"
+
+// 定数
+const WCHAR GITHUB_PR[] = L"GitHub PR";
+
+int wmain(int argc, WCHAR* argv[])
+{
+	GitHubPrApp app;
+	try {
+		// 最初の引数を取り出す
+		LPCWSTR arg = (1 < argc && argv[1][0] != '\0') ? argv[1] : L"";
+
+		// 初期化する
+		app.initSettings(arg);
+
+		//バッチファイルを実行する
+		app.executeBatch();
+
+		//ユーザ入力を待機する(読み捨て)
+		std::wcout << loadString(IDS_PRESS_ANY_KEY) << std::endl;
+		std::wcin.get();
+	}
+	catch (const std::runtime_error &e) {
+		const char* what = e.what();
+		const size_t cbWhat = ::strlen(what);
+		::MessageBox(NULL, convertMbsToWString(what, cbWhat).c_str(), GITHUB_PR, MB_OK);
+	}
+
+	return 0;
+}
+
