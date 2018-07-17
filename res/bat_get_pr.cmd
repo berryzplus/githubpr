28a035b04336993e6b52c0a6ca77edaf773aaa7b
berryplus <berryzplus@gmail.com>
2018-07-18 01:52:21 +0900

    ローカルブランチを削除するのをやめる
    * ローカルに同名ブランチが存在しない場合はfetchしてcheckout
    * ローカルに同名ブランチが存在する場合はcheckoutしてpull

--- res/bat_get_pr.cmd
+++ res/bat_get_pr.cmd
@@ -1,7 +1,8 @@
 @echo off
-git stash > NUL 2>&1
-git checkout %HOME_BRANCH% > NUL 2>&1
-git branch -D %BRANCH_NAME% > NUL 2>&1
-
-git fetch %GIT_REMOTE_NAME% pull/%PR_NUMBER%/head:%BRANCH_NAME%
 git checkout %BRANCH_NAME%
+IF ERRORLEVEL 1 (
+  git fetch %GIT_REMOTE_NAME% pull/%PR_NUMBER%/head:%BRANCH_NAME%
+  git checkout %BRANCH_NAME%
+) ELSE (
+  git pull %GIT_REMOTE_NAME% pull/%PR_NUMBER%/head
+)
