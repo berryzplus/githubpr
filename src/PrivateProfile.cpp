35bb578feb4b17c4f8397f79bd5cbc6c65a37852
berryplus <berryzplus@gmail.com>
2018-07-18 21:45:36 +0900

    PrivateProfile修正
    空文字列を取得した後にデバッグアサートが発生する問題に対処。

--- src/PrivateProfile.cpp
+++ src/PrivateProfile.cpp
@@ -4,8 +4,10 @@
 
 PrivateProfile::PrivateProfile(_In_ const std::wstring &profileName)
 	: _profileName(profileName.c_str())
-	, _buffer(0x800, '\0')
+	, _buffer(0x100, '\0')
 {
+	//初期値をセットする
+	_buffer._Eos(0x100);
 }
 
 PrivateProfile::PrivateProfile(_Inout_ _Myt &&other) noexcept
@@ -24,25 +26,24 @@ PrivateProfile& PrivateProfile::operator = (_Inout_ _Myt &&rhs) noexcept
 
 std::wstring PrivateProfile::getString(_In_z_ LPCWSTR appName, _In_z_ LPCWSTR keyName, _In_opt_z_ LPCWSTR pDefault)
 {
-	//moveで抜け殻になる対策
-	if (_buffer.size() == 0 && 0 < _buffer.capacity()) {
-		_buffer._Eos(1);
-		_buffer.at(0) = '\0';
-	}
 	SIZE_T cbLoad = 0;
 	for (;;) {
 		
+		// バッファから生ポインタとサイズ値を取り出す
+		WCHAR* buffer = &*_buffer.begin();
+		DWORD capacity = static_cast<DWORD>(_buffer.capacity());
+
 		// INIファイルの項目を取得
-		cbLoad = ::GetPrivateProfileString(appName, keyName, pDefault, &*_buffer.begin(), static_cast<DWORD>(_buffer.capacity()), _profileName);
+		cbLoad = ::GetPrivateProfileString(appName, keyName, pDefault, buffer, capacity, _profileName);
 
 		// メモリが十分に足りていた場合
-		if (cbLoad + 1 < _buffer.capacity()) {
-			_buffer._Eos(cbLoad);
+		if (cbLoad + 1 < capacity) {
+			buffer[cbLoad] = '\0'; //NUL終端する
 			break;
 		}
 
 		// メモリの再確保を行う
-		const SIZE_T cbNewBuffer = _buffer.capacity();
+		const SIZE_T cbNewBuffer = (capacity ^ 0xFFUL) * 2;
 		_buffer.reserve(cbNewBuffer);
 		if (_buffer.capacity() < cbNewBuffer) {
 			THROW_APP_EXCEPTION("can't read profile(realloc failed).");
