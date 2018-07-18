6a5eec105b2e565f5e754b663f21dad364dd28bd
berryplus <berryzplus@gmail.com>
2018-07-19 02:13:03 +0900

    操作結果が正しく出力されるようにバッチを修正。

--- res/bat_get_pr.cmd
+++ res/bat_get_pr.cmd
@@ -1,8 +1,10 @@
 @echo off
-git checkout %BRANCH_NAME%
+git checkout %BRANCH_NAME% > NUL 2>&1
 IF ERRORLEVEL 1 (
   git fetch %GIT_REMOTE_NAME% pull/%PR_NUMBER%/head:%BRANCH_NAME%
   git checkout %BRANCH_NAME%
 ) ELSE (
+  git checkout %HOME_BRANCH% > NUL 2>&1
+  git checkout %BRANCH_NAME%
   git pull %GIT_REMOTE_NAME% pull/%PR_NUMBER%/head
 )
