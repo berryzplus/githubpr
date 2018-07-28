e4eaf65c9557381f59d7c93818174e4af147107e
berryplus <berryzplus@gmail.com>
2018-07-28 22:09:45 +0900

    試しに書いてみたpythonソース

---
+++ res/make_pch_source.py
@@ -0,0 +1,15 @@
+﻿# -*- coding:utf-8 -*-
+import sys
+import pathlib
+args = sys.argv
+preCompiledHeaderName = "StdAfx" if len(args) <= 1 else args[1]
+preCompiledHeaderDir = "./" if len(args) <= 2 else args[2] + "/"
+preCompiledHeaderSource = preCompiledHeaderName + ".cpp"
+preCompiledHeaderPath = preCompiledHeaderDir + preCompiledHeaderSource
+if pathlib.Path(preCompiledHeaderPath).exists():
+    # do nothing
+    sys.exit()
+else:
+    with open(preCompiledHeaderPath, "w") as f:
+        print(f"// {preCompiledHeaderSource} : the Source-Code for Pre-Compiled Header", file=f)
+        print(f"#include \"{preCompiledHeaderName}.h\"", file=f)
